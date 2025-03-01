7b23fad56ff490a4852c490b785ee059b26202cb
Eta <pleasefckoff00@gmail.com>
2025-03-01 19:48:05 +0000

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,7 @@
+local places = {
+    [16732694052] = "https://raw.githubusercontent.com/ProjektEta/Vixie.lua/refs/heads/main/Scripts/Fisch.lua"
+}
 
+if places[game.PlaceId] then
+    loadstring(game:HttpGet(places[game.PlaceId]))()
+end
